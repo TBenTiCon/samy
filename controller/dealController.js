bdb1090f560953ca62312f66a19caebf2a94e391
Tician Streifeneder <t.streifeneder@benticon.de>
2021-05-05 18:00:07 +0200

    newQueryCheck

--- controller/dealController.js
+++ controller/dealController.js
@@ -3,25 +3,16 @@ const Company = require('../model/Company');
 const { handleError } = require('../middleware/errorHandler');
 
 module.exports.createDeal = async (req, res) => {
-	const { titel, subTitle, code, price, company, oldPrice, afLink, categorie } = req.body;
+	const { titel, subTitle, code, price, company, oldPrice, afLink, categorie, date } = req.body;
 
 	const myCompany = await Company.findOne({ name: company });
 
-	console.log(myCompany);
-
-	console.log('filePath: ' + req.file.path);
-
-	console.log('titel: ' + titel);
-	console.log('subTitel: ' + subTitle);
-
 	const imgLink = req.file.path.slice(7);
-	console.log('imgLink: ' + imgLink);
 
 	const cLink = await myCompany?.imgLink;
-	console.log(cLink);
 
 	try {
-		await Deal.create({ titel, subTitle, imgLink, cLink });
+		await Deal.create({ titel, subTitle, imgLink, cLink, date });
 		res.status(200).json({ status: `deal_created` });
 	} catch (err) {
 		handleError(err, res);
@@ -46,13 +37,31 @@ module.exports.getDeal = async (req, res) => {
 	const cat = req.query.cat;
 	const id = req.query.id;
 	const key = req.query.key;
+	const date = req.query.date;
+	const max = req.query.max;
 
-	console.log('getting deal key: ' + key);
+	let maxAmount = 10;
+
+	var conditions = {};
+
+	for (var key in req.query) {
+		if (req.query.hasOwnProperty(key)) {
+			conditions[key] = new RegExp('^' + req.query[key] + '$', 'i');
+		}
+	}
+
+	var query = Character.find(conditions);
+	query.exec(function (err, characters) {
+		if (err) throw err;
+		res.send({ characters: characters });
+	});
+
+	if (max) maxAmount = max;
 
 	try {
 		const deals = await Deal.find({
 			$or: [{ categorie: cat }, { _id: id }, { titel: { $regex: key, $options: 'i' } }],
-		}).limit(10);
+		}).limit(maxAmount);
 
 		console.log(deals);
 
