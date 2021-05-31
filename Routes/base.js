131d8cbcbc1464f70a2e880ff544f52b6b5697fe
Tician Streifeneder <t.streifeneder@benticon.de>
2021-05-31 20:32:37 +0200

    cropWorking

--- Routes/base.js
+++ Routes/base.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controller/baseController');
 
 router.get('/', controller.renderLanding);
+router.get('/crop', controller.renderCropping);
 
 module.exports = router;
--- controller/baseController.js
+++ controller/baseController.js
@@ -1,3 +1,7 @@
 module.exports.renderLanding = (req, res) => {
 	res.status(200).render('index');
 };
+
+module.exports.renderCropping = (req, res) => {
+	res.status(200).render('imgCrop');
+};
