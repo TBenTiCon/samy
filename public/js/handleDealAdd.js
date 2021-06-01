d7a359349c179cbaf22d2a2296d08993bacff615
Tician Streifeneder <t.streifeneder@benticon.de>
2021-06-01 16:34:44 +0200

    chanegsToNav

---
+++ public/js/handleDealAdd.js
@@ -0,0 +1,142 @@
+const form = document.querySelector('#dealForm');
+const btn = document.querySelector('#addDealBtn');
+let access_token = '';
+let id = false;
+const resultWrapper = document.querySelector('.result-wrapper');
+
+resultWrapper.addEventListener('click', async (e) => {
+	if (e.target?.className === 'editbtn') {
+		console.log('editClick');
+		id = e.target.dataset._id;
+
+		const data = await getFetchData(`deal/get/single?id=${id}`);
+
+		form.titel.value = data.status.titel;
+		form.subTitle.value = data.status.subTitle;
+		form.code.value = data.status.code;
+		form.price.value = data.status.price;
+		form.oldPrice.value = data.status.oldPrice;
+		form.afLink.value = data.status.afLink;
+		form.categorie.value = data.status.categorie;
+
+		btn.value = 'Aktualisieren';
+	}
+	if (e.target?.className === 'heading-8') {
+		const parent = e.target.parentElement;
+		id = parent.dataset._id;
+
+		const data = await getFetchData(`deal/get/single?id=${id}`);
+
+		form.titel.value = data.status.titel;
+		form.subTitle.value = data.status.subTitle;
+		form.code.value = data.status.code;
+		form.price.value = data.status.price;
+		form.oldPrice.value = data.status.oldPrice;
+		form.afLink.value = data.status.afLink;
+		form.categorie.value = data.status.categorie;
+
+		btn.value = 'Aktualisieren';
+	}
+});
+
+const updateDeal = async () => {
+	const formData = new FormData(form);
+
+	if (form.date) formData.set('date', convertTimeToDays(new Date(form.date.value)));
+	if (form.time) formData.set('time', convertToMinutes(form.time.value));
+
+	if (form.down) formData.set('down', convertTimeToDays(new Date(form.down.value)));
+	if (form.down_time) formData.set('down_time', convertToMinutes(form.down_time.value));
+
+	for (var pair of formData.entries()) {
+		console.log(pair[0] + ', ' + pair[1]);
+	}
+
+	multiPartFetch(formData, `deal/create?id=${id}`)
+		.then((data) => {
+			console.log('success');
+			window.location.href = '/deal/create';
+			id = false;
+		})
+		.catch((err) => {
+			console.log(err);
+			btn.value = 'Fehler - Falscheingaben überprüfen';
+			id = false;
+		});
+};
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	if (!id) {
+		const formData = new FormData(form);
+
+		var selected = new Date(form.date.value);
+
+		var date = convertTimeToDays(selected);
+		var time = convertToMinutes(form.time.value);
+
+		formData.set('date', date);
+
+		formData.set('time', time);
+
+		formData.set('access_token', access_token);
+
+		formData.set('down', convertTimeToDays(new Date(form.down.value)));
+		formData.set('down_time', convertToMinutes(form.down_time.value));
+
+		for (var pair of formData.entries()) {
+			console.log(pair[0] + ', ' + pair[1]);
+		}
+
+		multiPartFetch(formData, 'deal/create')
+			.then((data) => {
+				//console.log('success');
+				window.location.href = '/deal/create';
+			})
+			.catch((err) => {
+				console.log(err);
+				btn.value = 'Fehler - Falscheingaben überprüfen';
+			});
+	} else if (id) {
+		updateDeal();
+	}
+});
+
+function checkLoginState() {
+	FB.getLoginStatus(function (response) {
+		if (response.status === 'connected') {
+			access_token = response.authResponse.accessToken;
+
+			//handleFBPost();
+		}
+	});
+}
+
+const handleFBPost = async () => {
+	const IGKey = await fetch(
+		`https://graph.facebook.com/v10.0/105151565090283?fields=instagram_business_account&access_token=${access_token}`,
+		{
+			method: 'GET',
+		}
+	);
+
+	const Key = await IGKey.json();
+
+	const uploadIG = await fetch(
+		`https://graph.facebook.com/${Key.instagram_business_account?.id}/media?image_url=https://upload.wikimedia.org/wikipedia/commons/0/0e/Tree_example_VIS.jpg&caption="Hello WorldText"&access_token=${access_token}`,
+		{ method: 'POST' }
+	);
+
+	const IGPOSTContainer = await uploadIG.json();
+	const PostID = await IGPOSTContainer.id;
+	console.log(PostID);
+
+	//POST IMG
+	const postIG = await fetch(
+		`https://graph.facebook.com/${Key.instagram_business_account?.id}/media_publish?creation_id=${PostID}&access_token=${access_token}`,
+		{ method: 'POST' }
+	);
+
+	console.log(await postIG.json());
+};
--- public/js/renderDeals.js
+++ public/js/renderDeals.js
@@ -2,7 +2,6 @@ const dealWrapper = document.querySelector('.result-wrapper');
 const searchBar = document.querySelector('.dealsearchbar');
 const searchForm = document.querySelector('#search_form');
 const heading4 = document.querySelector('.heading-4');
-const catWrapper = document.querySelector('.cat-wrapper');
 const mobileCatWrapper = document.querySelector('.mobile-cat-wrapper');
 const catNav = document.querySelector('.cat_nav');
 
@@ -35,38 +34,101 @@ const renderDeals = (dealsArray) => {
 			//console.log(convertTimeToDays(new Date()) + '=>' + deal.down);
 
 			if (convertTimeToDays(new Date()) === deal.down) {
-				console.log('deal.down:' + deal.down);
 				console.log('downable');
 				isDownable = true;
 			}
 
-			console.log('isDownable: ' + isDownable);
-
 			dealWrapper.innerHTML += `
-
-			<a href="#" class="dealitem w-inline-block"
-			><img src="${deal.imgLink}" loading="lazy" object-fit="cover" alt="" class="dealbackground" />
-			<div class="img_overlay"></div>
-			<div class="countdown-container ${isDownable ? 'show' : 'hidden'}" id="I${deal._id}">
-				<h4 class="heading-17 timermin">00</h4>
-				<h4 class="heading-17">:</h4>
-				<h4 class="heading-17 timersec">00</h4>
-				<h4 class="heading-17">:</h4>
-				<h4 class="heading-17 timersec">00</h4>
-			</div>
-			<div class="div-block-12">
-				<div class="result-content">
-					<div class="div-block-33">
-						<div class="div-block-34">
-							<h4 class="result_heading">${deal.titel}</h4>
+            <div class="result-item">
+					<div class="div-block-12">
+						<div class="countdown-container ${isDownable ? 'show' : 'hidden'}" id="I${deal._id}">
+							<h4 class="heading-17 timermin">00</h4>
+							<h4 class="heading-17">:</h4>
+							<h4 class="heading-17 timersec">00</h4>
+							<h4 class="heading-17">:</h4>
+							<h4 class="heading-17 timerSeconds">00</h4>
+						</div>
+						<img src="${deal.imgLink}" loading="lazy" alt="" class="dealimg" />
+						<div class="result-content">
+							<div>
+								<h6 class="result_time">${dayToDate(deal.date)} | ${convertToTime(deal.time)} Uhr</h6>
+								<h4 class="result_heading">${deal.titel}</h4>
+								<h4 class="subheading">${deal.subTitle}</h4>
+								<h4 class="codeline">Gutschein: ${deal.code ? deal.code : 'Nicht benötigt'}</h4>
+							</div>
+							<div class="div-block-6">
+								<div class="div-block-7">
+									<img
+										src=${deal.cLink}
+										loading="lazy"
+										sizes="(max-width: 479px) 100vw, (max-width: 991px) 128px, 165px"
+										srcset="
+											images/DealSale-Logo-p-500.png   500w,
+											images/DealSale-Logo-p-800.png   800w,
+											images/DealSale-Logo-p-1080.png 1080w,
+											images/DealSale-Logo.png        1441w
+										"
+										alt=""
+										class="result_company"
+									/>
+									<div class="result-meta" data-interacted="false">
+										<div class="div-block-28">
+											<div class="html-embed w-embed">
+												<div class="fb-share-button" data-afLink=${deal.afLink}></div>
+											</div>
+											<div class="sharelink" data-afLink=${deal.afLink}></div>
+										</div>
+										<div class="likebtncon">
+											<h5 class="likeamount">${deal.likes}</h5>
+											<img src="/images/like.svg" loading="lazy" alt="" class="like_img" data-_id="${deal._id}" data-interacted="false"/>
+										</div>
+										<div class="likebtncon">
+											<h5 class="likeamount">${deal.dislikes}</h5>
+											<img src="/images/dislike.svg" loading="lazy" alt="" class="dislike_img" data-_id="${
+												deal._id
+											}" data-interacted="false"/>
+										</div>
+									</div>
+								</div>
+								<div class="div-block-8">
+									<h3 class="deal-price">${deal.price.replace('.', ',')}€</h3>
+									<div class="div-block-9">
+										<h4 class="oldprice">${deal.oldPrice.replace('.', ',')} €</h4>
+										<h4 class="percentcalc">(-${Math.round(100 - parseFloat(deal.price) / (parseFloat(deal.oldPrice) / 100))}%)</h4>
+									</div>
+									<div class="todealbtn">
+										<h4 class="heading-8"><a href="${deal.afLink}" style="text-decoration: none; color:white">Zum Deal</a></h4>
+										<div class="div-block-11"></div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
-					<h6 class="result_time">${dayToDate(deal.date)} | ${convertToTime(deal.time)} Uhr</h6>
-					<h4 class="codeline">${deal.code ? deal.code : 'Nicht benötigt'}</h4>
-				</div>
-			</div>
-			</a>
-`;
+					<div class="div-block-13">
+						<div class="result-meta" data-interacted="false">
+							<div class="div-block-28">
+								<div class="html-embed w-embed">
+									<div class="fb-share-button" data-afLink=${deal.afLink}></div>
+								</div>
+								<div class="sharelink" data-afLink=${deal.afLink}></div>
+							</div>
+							<div class="likebtncon" >
+								<h5 class="likeamount">1</h5>
+								<img src="images/like.svg" loading="lazy" alt="like button" class="like_img " data-_id="${
+									deal._id
+								}" data-interacted="false"/>
+							</div>
+							<div class="likebtncon">
+								<h5 class="likeamount">1</h5>
+								<img src="images/dislike.svg" loading="lazy" alt="" class="dislike_img" data-_id=${deal._id} data-interacted="false"/>
+							</div>
+						</div>
+						<div class="todealbtn mobile">
+							<h4 class="heading-8"><a href=${deal.afLink} style="text-decoration: none">Zum Deal</a></h4>
+							<div class="div-block-11"></div>
+						</div>
+					</div>
+				</div> `;
 
 			//setTimer
 
@@ -85,6 +147,84 @@ const renderDeals = (dealsArray) => {
 			timer.countDown();
 		});
 	}
+
+	const shareBtn = document.getElementsByClassName('fb-share-button');
+	const shareBtns = Array.from(shareBtn);
+
+	shareBtns.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			if (e.target?.className === 'fb-share-button') {
+				//console.log(e.target.dataset.aflink);
+
+				FB.ui(
+					{
+						method: 'share',
+						href: `${e.target.dataset.aflink}`,
+					},
+					function (response) {}
+				);
+			}
+		});
+	});
+
+	const like_img = document.getElementsByClassName('like_img');
+	const likeBtns = Array.from(like_img);
+
+	likeBtns.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			if (e.target?.className === 'like_img') {
+				if (e.target.dataset.interacted === 'false') {
+					console.log('like');
+
+					if (e.target.parentElement.parentElement.dataset.interacted === 'false') {
+						console.log('like2');
+
+						console.log(e.target);
+						postFetchData({}, `deal/like?id=${e.target.dataset._id}`);
+
+						const likeDisplay = e.target.parentElement.querySelector('h5');
+						likeDisplay.textContent = parseInt(likeDisplay.textContent) + 1;
+
+						e.target.dataset.interacted = 'true';
+						e.target.parentElement.parentElement.dataset.interacted = 'true';
+					}
+				}
+			}
+		});
+	});
+
+	const dislike_img = document.getElementsByClassName('dislike_img');
+	const dislikeBtns = Array.from(dislike_img);
+
+	dislikeBtns.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			if (e.target.dataset.interacted === 'false') {
+				if (e.target.parentElement.parentElement.dataset.interacted === 'false') {
+					console.log(e.target);
+					postFetchData({}, `deal/dislike?id=${e.target.dataset._id}`);
+
+					const likeDisplay = e.target.parentElement.querySelector('h5');
+					likeDisplay.textContent = parseInt(likeDisplay.textContent) + 1;
+
+					e.target.dataset.interacted = 'true';
+					e.target.parentElement.parentElement.dataset.interacted = 'true';
+				}
+			}
+		});
+	});
+
+	const linkButtons = document.getElementsByClassName('sharelink');
+	const linkBtns = Array.from(linkButtons);
+
+	linkBtns.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			if (e.target?.className === 'sharelink') {
+				const text = e.target.dataset.aflink;
+				copyToClipboard(text);
+				window.alert('Link zur Zwischenablage hinzugefügt');
+			}
+		});
+	});
 };
 
 window.onload = () => {
@@ -172,10 +312,42 @@ const searchCat = (e) => {
 };
 
 mobileCatWrapper.addEventListener('click', (e) => {
-	console.log('click!');
 	searchCat(e);
 });
 
 catNav.addEventListener('click', (e) => {
 	searchCat(e);
+});
+
+function copyToClipboard(text) {
+	if (window.clipboardData && window.clipboardData.setData) {
+		// Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+		return window.clipboardData.setData('Text', text);
+	} else if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+		var textarea = document.createElement('textarea');
+		textarea.textContent = text;
+		textarea.style.position = 'fixed'; // Prevent scrolling to bottom of page in Microsoft Edge.
+		document.body.appendChild(textarea);
+		textarea.select();
+		try {
+			return document.execCommand('copy'); // Security exception may be thrown by some browsers.
+		} catch (ex) {
+			console.warn('Copy to clipboard failed.', ex);
+			return false;
+		} finally {
+			document.body.removeChild(textarea);
+		}
+	}
+}
+
+const NavBtn = document.querySelector('.div-block-15');
+NavBtn.addEventListener('click', () => {
+	console.log('click');
+	document.querySelector('.mobile_nav').classList.add('showMenu');
+});
+
+const closeNavBtn = document.querySelector('.div-block-14');
+closeNavBtn.addEventListener('click', () => {
+	console.log('click');
+	document.querySelector('.mobile_nav').classList.remove('showMenu');
 });
