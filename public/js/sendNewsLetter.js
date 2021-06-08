b0dfad6b1c3c83f779364799b5d9a229bdaa1788
Tician Streifeneder <t.streifeneder@benticon.de>
2021-06-08 17:01:23 +0200

    testNL

--- public/js/sendNewsLetter.js
+++ public/js/sendNewsLetter.js
@@ -4,7 +4,7 @@ let number = 0;
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	if (number >= 16) {
+	if (number >= 0) {
 		postFetchData({}, `sendMails`).then((data) => {
 			console.log(data);
 
