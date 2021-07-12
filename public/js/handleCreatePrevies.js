5fd8b1771dd7b1f4ac316914c8fd1645e17a1c30
Tician Streifeneder <t.streifeneder@benticon.de>
2021-07-12 12:31:10 +0200

    t

--- public/js/handleCreatePrevies.js
+++ public/js/handleCreatePrevies.js
@@ -1,5 +1,5 @@
 const prevForm = document.querySelector('#dealForm');
-const title = prevForm.title;
-const subTitle = dealForm.subTitle;
+const title = prevForm.querySelector('#title');
+const subTitle = dealForm.querySelector('#subTitle');
 
 console.log('title:' + title);
